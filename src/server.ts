0f33429883bf6ec60d18b277cb56738990a6a13f
Lucas Gabriel da Silva <gabrieldasilvalucas418@gmail.com>
2025-08-29 12:33:10 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Olá, mundo!");
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Página Home');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor em execução em http://localhost:${port}`);
+})
