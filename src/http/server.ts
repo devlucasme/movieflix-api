36598e4235396959704a5cabadd4de5c4916a201
Lucas Gabriel da Silva <gabrieldasilvalucas418@gmail.com>
2025-09-06 16:15:48 -0300

    adicionado swagger com cadastro de filmes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import MoviesRoutes from '../routes/movies.routes';
+import SwaggerUiOptions from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use('/docs', SwaggerUiOptions.serve, SwaggerUiOptions.setup(swaggerDocument));
 
 app.use('/movies', MoviesRoutes);
 
