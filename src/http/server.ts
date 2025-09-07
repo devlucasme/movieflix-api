4e900af21583bdf241da322723233058f6a8b906
Lucas Gabriel da Silva <gabrieldasilvalucas418@gmail.com>
2025-09-06 22:09:35 -0300

    adicionado cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 import MoviesRoutes from '../routes/movies.routes';
 import SwaggerUiOptions from 'swagger-ui-express';
 import swaggerDocument from '../../swagger.json';
+import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/docs', SwaggerUiOptions.serve, SwaggerUiOptions.setup(swaggerDocument));
 
